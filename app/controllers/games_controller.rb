2b1c84396d2bc916a914f238db5a7bc7c49d8ff3
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-14 16:27:48 +0100

    Longest word with total score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,42 +1,36 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    chars = ('A'..'Z').to_a
-    @letters = (0...10).map { chars.sample }
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
     @letters = params[:letters]
     @word = params[:word].upcase
 
-    buildable = buildable?(@word.split(''), @letters.split(''))
+    buildable = @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) }
 
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     found = JSON.parse(URI.open(url).read)['found']
 
-    @result = result(buildable, found)
+    @result = result(buildable, found, @word.length)
+
+    session[:total_score] = (session[:total_score] || 0) + @score
   end
 
   private
 
-  def buildable?(word, letters)
-    buildable = true
-    word.each do |w|
-      w_index = letters.index(w)
-      if w_index
-        letters.delete_at(w_index)
-      else
-        buildable = false
-      end
-    end
-    buildable
-  end
-
-  def result(buildable, found)
+  def result(buildable, found, length)
     if buildable && found
+      @score = length**2
       'won'
     elsif found
+      @score = 0
       'not buildable'
     else
+      @score = 0
       'not in dict'
     end
   end
